be270370aa68c07ef63c4ba61e969094f1806d53
Andrey Valujko <Andrey.Valujko@softeq.com>
2022-11-09 01:04:24 +0200

    add Device class

---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Domain/AggregatesModel/DeviceAggregate/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialsSystem.UsersService.Domain.AggregatesModel.DeviceAggregate
+{
+    internal class Device
+    {
+        public string Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public DeviceType Type { get; set; }
+        public string FirmwareVersion { get; set; }
+    }
+}
---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Domain/AggregatesModel/DeviceAggregate/DeviceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialsSystem.UsersService.Domain.AggregatesModel.DeviceAggregate
+{
+    internal class DeviceType
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
